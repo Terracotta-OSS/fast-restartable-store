989ab4ec71e02d51e9d4f8062039ac5a7734ee45
Dennis Chris <Chris.Dennis@softwareag.com>
2012-03-29 18:28:07 +0000

    add TC copyright header to source files
    svn path=/fast-restartable-store/trunk/; revision=16395

--- src/test/java/com/terracottatech/frs/mock/transaction/MockTransactionLockProvider.java
+++ src/test/java/com/terracottatech/frs/mock/transaction/MockTransactionLockProvider.java
@@ -1,3 +1,7 @@
+/*
+ * All content copyright (c) 2012 Terracotta, Inc., except as may otherwise
+ * be noted in a separate copyright notice. All rights reserved.
+ */
 package com.terracottatech.frs.mock.transaction;
 
 import java.util.ArrayList;

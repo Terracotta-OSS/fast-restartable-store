868bc662b9c21dca673fdcac713c715f2e33de60
Myron Scott <myron.scott@softwareag.com>
2012-06-22 22:25:35 +0000

    make the default segment size 512MB
    svn path=/fast-restartable-store/trunk/; revision=18413

--- src/main/java/com/terracottatech/frs/config/FrsProperty.java
+++ src/main/java/com/terracottatech/frs/config/FrsProperty.java
@@ -7,7 +7,7 @@ public enum FrsProperty {
   IO_COMMITLIST("io.commitList", Type.STRING, "ATOMIC"),
   IO_WAIT("io.wait", Type.INTEGER, 200),
   
-  IO_NIO_SEGMENT_SIZE("io.nio.segmentSize", Type.LONG, 16L * 1024 * 1024),
+  IO_NIO_SEGMENT_SIZE("io.nio.segmentSize", Type.LONG, 512L * 1024 * 1024),
   IO_NIO_MEMORY_SIZE("io.nio.memorySize", Type.LONG, ((Long) IO_NIO_SEGMENT_SIZE.defaultValue()) * 4),
   IO_NIO_MEMORY_SPINS("io.nio.memorySpinsToFail", Type.INTEGER, 6),
   IO_NIO_MEMORY_TIMEOUT("io.nio.memoryTimeout", Type.LONG, 250L),

5da2f03cd7c0d32b126dc5aedeb9c8a7e70f5847
Myron Scott <myron.scott@softwareag.com>
2013-09-04 16:45:21 +0000

    create random access by default for tests
    svn path=/fast-restartable-store/trunk/; revision=30601

--- src/main/java/com/terracottatech/frs/io/nio/NIOManager.java
+++ src/main/java/com/terracottatech/frs/io/nio/NIOManager.java
@@ -66,11 +66,11 @@ public class NIOManager implements IOManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(IOManager.class);
 
     public NIOManager(String home, long segmentSize) throws IOException {
-        this(home,segmentSize,segmentSize * 4, false);
+        this(home,segmentSize,segmentSize * 4, true);
     }    
      
     public NIOManager(String home, long segmentSize, long memorySize) throws IOException {
-        this(home,segmentSize,memorySize,false);
+        this(home,segmentSize,memorySize,true);
     }
      
     public NIOManager(String home, long segmentSize, long memorySize,boolean randomAccess) throws IOException {

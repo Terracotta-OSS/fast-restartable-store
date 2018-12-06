78de74e569d18eda5ff9198c9cec79492a3aad61
Ramesh Kavanappillil <Ramesh.Kavanappillil@softwareag.com>
2018-12-06 09:51:03 +0000

    TDB-3785 Added tracing to debug 3785
    svn path=/fast-restartable-store/trunk/; revision=53858

--- src/main/java/com/terracottatech/frs/io/nio/FileChannelReadOpener.java
+++ src/main/java/com/terracottatech/frs/io/nio/FileChannelReadOpener.java
@@ -4,12 +4,17 @@
  */
 package com.terracottatech.frs.io.nio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 class FileChannelReadOpener implements ChannelOpener {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FileChannelReadOpener.class);
   private final File fileToOpen;
   private FileInputStream currentStream;
   private volatile boolean closed;
@@ -49,6 +54,18 @@ class FileChannelReadOpener implements ChannelOpener {
   // explicit close
   @Override
   public synchronized void close() throws IOException {
+    if (LOGGER.isTraceEnabled()) {
+      // For TDB-3758: to check where the premature close is coming from
+      LOGGER.trace("Close invoked from ", new Exception());
+      final StringBuilder stackTrace = new StringBuilder();
+      stackTrace.append("DUMPING ENTIRE STACK").append(System.lineSeparator());
+      // dumping stack trace
+      Thread.getAllStackTraces().entrySet().stream().map((v) -> {
+        stackTrace.append("Thread ID: ").append(v.getKey().getName()).append(" STATE: ").append(v.getKey().getState()).append(System.lineSeparator());
+        return Arrays.asList(v.getValue());
+      }).forEach((l) -> l.stream().map(StackTraceElement::toString).forEach((s) -> stackTrace.append(s).append(System.lineSeparator())));
+      LOGGER.trace(stackTrace.toString());
+    }
     closed = true;
     if (currentStream != null) {
       try {
